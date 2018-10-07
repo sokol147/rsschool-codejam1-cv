1f626448b99b72c38f04e677ab07b90cf3580408
Sokolov V <sokolov.stb@gmail.com>
2018-10-07 15:42:12 +0300

    feat: add mobile version

--- build/templates/js/app.js
+++ build/templates/js/app.js
@@ -1,2 +1,10 @@
+let btn = document.getElementById('menu-btn');
+let menu = document.getElementById('nav');
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIiXX0=
+btn.onclick = function(event){
+  event.preventDefault();
+
+  btn.classList.toggle('menu-btn--active');
+  menu.classList.toggle('open');
+}
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYXBwLmpzIiwic291cmNlc0NvbnRlbnQiOlsibGV0IGJ0biA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdtZW51LWJ0bicpO1xyXG5sZXQgbWVudSA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCduYXYnKTtcclxuXHJcbmJ0bi5vbmNsaWNrID0gZnVuY3Rpb24oZXZlbnQpe1xyXG4gIGV2ZW50LnByZXZlbnREZWZhdWx0KCk7XHJcblxyXG4gIGJ0bi5jbGFzc0xpc3QudG9nZ2xlKCdtZW51LWJ0bi0tYWN0aXZlJyk7XHJcbiAgbWVudS5jbGFzc0xpc3QudG9nZ2xlKCdvcGVuJyk7XHJcbn0iXX0=
